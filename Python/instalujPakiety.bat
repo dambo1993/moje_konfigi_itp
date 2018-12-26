35fc1fd9f08bd512f14c92262647c910b46b6362
Przemyslaw Michalak <thedambo1@gmail.com>
2018-12-26 13:41:57 +0100

    python pakiety - dodanie pandas + aktualizacja pip

--- Python/instalujPakiety.bat
+++ Python/instalujPakiety.bat
@@ -1,5 +1,7 @@
+python -m pip install --upgrade pip
 python -m pip install beautifultable
 python -m pip install readchar
 python -m pip install colored
+python -m pip install pandas
 
 @set /p DUMMY=Hit ENTER to continue...
