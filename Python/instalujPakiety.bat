8d179f75c4e40b14db7770d1ae3611fe99e441cb
Przemyslaw Michalak <thedambo1@gmail.com>
2018-12-27 23:53:44 +0100

    python pakiety - dodanie docopt!

--- Python/instalujPakiety.bat
+++ Python/instalujPakiety.bat
@@ -3,5 +3,6 @@ python -m pip install beautifultable
 python -m pip install readchar
 python -m pip install colored
 python -m pip install pandas
+python -m pip install docopt
 
 @set /p DUMMY=Hit ENTER to continue...
