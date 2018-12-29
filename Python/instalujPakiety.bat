0031f54dcadc5f1868b912c78e639df288ccf780
Przemyslaw Michalak <thedambo1@gmail.com>
2018-12-29 22:46:28 +0100

    python pakiety - clipboard dodany do skryptu instalacyjnego

--- Python/instalujPakiety.bat
+++ Python/instalujPakiety.bat
@@ -4,5 +4,6 @@ python -m pip install readchar
 python -m pip install colored
 python -m pip install pandas
 python -m pip install docopt
+python -m pip install clipboard
 
 @set /p DUMMY=Hit ENTER to continue...
