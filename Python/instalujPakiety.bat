6d3b74cd9d426244600c036775113f3ab4e290b0
Przemysław Michalak <p.michalak@fibargroup.com>
2019-01-24 15:46:37 +0100

    python - auto exe

--- Python/instalujPakiety.bat
+++ Python/instalujPakiety.bat
@@ -5,5 +5,5 @@ python -m pip install colored
 python -m pip install pandas
 python -m pip install docopt
 python -m pip install clipboard
-
+python -m pip install auto-py-to-exe
 @set /p DUMMY=Hit ENTER to continue...
