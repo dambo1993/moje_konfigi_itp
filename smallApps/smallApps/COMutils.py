842a92d9e71aee4b3377c763df96ff8ca3895e72
Przemyslaw Michalak <thedambo1@gmail.com>
2019-02-01 23:54:59 +0100

    SA - dodanie info o zajetosci portu + ladniejszy output

--- smallApps/smallApps/COMutils.py
+++ smallApps/smallApps/COMutils.py
@@ -1,17 +1,37 @@
 import serial.tools.list_ports
 from beautifultable import BeautifulTable
+from colored import fg, bg, attr
+def checkPortBusy(port):
+    try:
+       ser = serial.Serial(port)
+       return False
+    except:
+       return True
+
+def portStateString(port):
+	state = "Free"
+	color = 'green'
+	if checkPortBusy(port):
+		state = "BUSY"
+		color = 'red'
+	return fg(color)+state+attr(1)
+
+def descStr(port, desc):
+	return desc[:desc.find("(")].strip()
+
+def nameStr(hwid):
+	if "USB VID:PID=0403:6001" in hwid:
+		return "FT232"
+	if "USB VID:PID=1A86:7523" in hwid:
+		return "CH340"
+	return "Unknown"
 
 def printPortsInfo(ports, name=""):
 	print(name)
 	table = BeautifulTable()
-	table.column_headers = ["N", "PORT", "DESCRIPTION", "HWID", "NAME"]
+	table.column_headers = ["N", "PORT", "STATE", "DESCRIPTION", "HWID", "NAME"]
 	number = 1
 	for port, desc, hwid in ports:
-		devName = "?"
-		if "USB VID:PID=0403:6001" in hwid:
-			devName = "FT232"
-		if "USB VID:PID=1A86:7523" in hwid:
-			devName = "CH340"
-		table.append_row([number, port, desc, hwid, devName])
+		table.append_row([number, port, portStateString(port) , descStr(port, desc), hwid.replace("  "," ").replace(" ","\n"), nameStr(hwid)])
 		number += 1
 	print(table)
