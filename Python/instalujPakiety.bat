490b952cc64f78ee2c26db4ffd56fcc68340a9fd
Przemysław Michalak <p.michalak@fibargroup.com>
2019-02-04 11:41:37 +0100

    python - autoupgrade libs

--- Python/instalujPakiety.bat
+++ Python/instalujPakiety.bat
@@ -1,9 +1,9 @@
 python -m pip install --upgrade pip
-python -m pip install beautifultable
-python -m pip install readchar
-python -m pip install colored
-python -m pip install pandas
-python -m pip install docopt
-python -m pip install clipboard
-python -m pip install auto-py-to-exe
+python -m pip install --upgrade beautifultable
+python -m pip install --upgrade readchar
+python -m pip install --upgrade colored
+python -m pip install --upgrade pandas
+python -m pip install --upgrade docopt
+python -m pip install --upgrade clipboard
+python -m pip install --upgrade auto-py-to-exe
 @set /p DUMMY=Hit ENTER to continue...
