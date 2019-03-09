1cc209b6ce3dfef82dff0a5d95e0d9a50c6abf83
Przemyslaw Michalak <test.txt>
2019-03-09 11:11:31 +0100

    small apps - csectionformatter - refactor with center python function

--- smallApps/smallApps/cSectionFormatter.py
+++ smallApps/smallApps/cSectionFormatter.py
@@ -5,17 +5,9 @@ length = 120
 
 text = sys.argv[1]
 
-spacesCounter = length - len(text) - 4
-
-spaces1 = int(spacesCounter/2)
-
-if spacesCounter % 2 == 0:
-	spaces2 = spaces1
-else:
-	spaces2 = spaces1 + 1
-
 lineSeparators = "/" + ((length - 2) * "*") + "/"
-linetxt = "/*" + (spaces1 * " ") + text + (spaces2 * " ") + "*/"
+
+linetxt = "/*" + text.center(length - 4) + "*/"
 
 output = lineSeparators + "\n" + linetxt + "\n" + lineSeparators
 
